47e318c0ae818a325f2558f53dfc9ba2abb1bf8f
sawantdhananjay <dhananjay@pixelbazar.com>
2019-07-19 11:16:16 +0530

    comment

--- application/views/pages/index.php
+++ application/views/pages/index.php
@@ -268,7 +268,7 @@
 
       </div>
     <div class="col-md-8  service-row-text">
-      <div class="tabcontent  shadow image-default ">
+      <!-- <div class="tabcontent  shadow image-default ">
         <div class="cancel float-right" onclick="alert();"><svg  x="0" y="200" width="60" height="20" viewBox="0 0 352 512"><path fill="currentColor" d="M242.72 256l100.07-100.07c12.28-12.28 12.28-32.19 0-44.48l-22.24-22.24c-12.28-12.28-32.19-12.28-44.48 0L176 189.28 75.93 89.21c-12.28-12.28-32.19-12.28-44.48 0L9.21 111.45c-12.28 12.28-12.28 32.19 0 44.48L109.28 256 9.21 356.07c-12.28 12.28-12.28 32.19 0 44.48l22.24 22.24c12.28 12.28 32.2 12.28 44.48 0L176 322.72l100.07 100.07c12.28 12.28 32.2 12.28 44.48 0l22.24-22.24c12.28-12.28 12.28-32.19 0-44.48L242.72 256z" class=""></path></svg></div>
         <div class="sub-tabcontent">
         <img class="service-image" src="<?php echo base_url(); ?>assets/images/site/webs.gif" alt="">
@@ -278,7 +278,7 @@
           <a class="btn btn-md btn-outline-warning2 margin-top" href="<?php echo base_url(); ?>Contact">JUST DROP US MAIL</a>
       </p>
         </div>
-      </div>
+      </div> -->
       <div id="London" class="tabcontent shadow ">
         <div class="cancel float-right" onclick="alert();"><svg  x="0" y="200" width="60" height="20" viewBox="0 0 352 512"><path fill="currentColor" d="M242.72 256l100.07-100.07c12.28-12.28 12.28-32.19 0-44.48l-22.24-22.24c-12.28-12.28-32.19-12.28-44.48 0L176 189.28 75.93 89.21c-12.28-12.28-32.19-12.28-44.48 0L9.21 111.45c-12.28 12.28-12.28 32.19 0 44.48L109.28 256 9.21 356.07c-12.28 12.28-12.28 32.19 0 44.48l22.24 22.24c12.28 12.28 32.2 12.28 44.48 0L176 322.72l100.07 100.07c12.28 12.28 32.2 12.28 44.48 0l22.24-22.24c12.28-12.28 12.28-32.19 0-44.48L242.72 256z" class=""></path></svg></div>
         <div class="sub-tabcontent">
