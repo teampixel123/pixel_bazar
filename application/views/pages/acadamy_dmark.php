296e5f1ea2cb852bcff5ee9900687933428e7d7b
sawantdhananjay <dhananjay@pixelbazar.com>
2019-09-10 18:08:01 +0530

    web digital marketing page and link

---
+++ application/views/pages/acadamy_dmark.php
@@ -0,0 +1,249 @@
+<?php
+$title="Web Design and Development in Kolhapur | PixelBazar";
+$description="Best Rated Website Designing Company in Kolhapur. We Create Variety of Websites for all Platforms. &#10004; 100% Mobile Responsive &#10004; Static &#10004; Dynamic &#10004; E-Commerce. Order Your Website Now !";
+$keywords="website design, web design, web page design, website development, web design company, web development company, ecommerce website, static website, dynamic website, web design company in kolhapur, website design company in kolhapur, web development company in kolhapur, website development company in kolhapur, web design services";
+$active='service';
+include('head.php'); ?>
+<body class="woocommerce pc">
+	<!-- page header -->
+  <?php include('header.php'); ?>
+
+  <div class="container service-pg-container">
+
+    <!-- <div class="alert alert-success w-50" role="alert">
+      This is a success alert—check it out!
+    </div>
+    <div class="alert alert-danger  w-50" role="alert">
+      This is a danger alert—check it out!
+    </div> -->
+
+    <div class="row ">
+      <div class="col-md-8" id="left-col">
+          <h1 class="service-title">Beautify you website with Web design and development services </h1>
+          <h3 class="service-sub-title red-color">Turn visitors into customers with beautiful designs</h3>
+          <div class="social-media">
+            <!-- <?php echo $email_msg; ?> -->
+            <a target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=<?php echo base_url(); ?>website-design-development-kolhapur">
+              <div class="media-round fb-color-bg">
+                <i  class="">
+                  <svg style="width:10px; padding-bottom:7px;" viewBox="0 0 320 512">
+                    <path fill="currentColor" d="M279.14 288l14.22-92.66h-88.91v-60.13c0-25.35 12.42-50.06 52.24-50.06h40.42V6.26S260.43 0 225.36 0c-73.22 0-121.08 44.38-121.08 124.72v70.62H22.89V288h81.39v224h100.17V288z" class=""></path>
+                  </svg>
+                </i>
+              </div>
+            </a>
+
+            <a class="display_mobile" id="whats_app" target="_blank" href="https://web.whatsapp.com/send?text=<?php echo base_url(); ?>website-design-development-kolhapur">
+              <div class="media-round wa-color-bg">
+                <i class="">
+                  <svg style="width:18px; padding-bottom:6px;" viewBox="0 0 448 512" ><path fill="currentColor" d="M380.9 97.1C339 55.1 283.2 32 223.9 32c-122.4 0-222 99.6-222 222 0 39.1 10.2 77.3 29.6 111L0 480l117.7-30.9c32.4 17.7 68.9 27 106.1 27h.1c122.3 0 224.1-99.6 224.1-222 0-59.3-25.2-115-67.1-157zm-157 341.6c-33.2 0-65.7-8.9-94-25.7l-6.7-4-69.8 18.3L72 359.2l-4.4-7c-18.5-29.4-28.2-63.3-28.2-98.2 0-101.7 82.8-184.5 184.6-184.5 49.3 0 95.6 19.2 130.4 54.1 34.8 34.9 56.2 81.2 56.1 130.5 0 101.8-84.9 184.6-186.6 184.6zm101.2-138.2c-5.5-2.8-32.8-16.2-37.9-18-5.1-1.9-8.8-2.8-12.5 2.8-3.7 5.6-14.3 18-17.6 21.8-3.2 3.7-6.5 4.2-12 1.4-32.6-16.3-54-29.1-75.5-66-5.7-9.8 5.7-9.1 16.3-30.3 1.8-3.7.9-6.9-.5-9.7-1.4-2.8-12.5-30.1-17.1-41.2-4.5-10.8-9.1-9.3-12.5-9.5-3.2-.2-6.9-.2-10.6-.2-3.7 0-9.7 1.4-14.8 6.9-5.1 5.6-19.4 19-19.4 46.3 0 27.3 19.9 53.7 22.6 57.4 2.8 3.7 39.1 59.7 94.8 83.8 35.2 15.2 49 16.5 66.6 13.9 10.7-1.6 32.8-13.4 37.4-26.4 4.6-13 4.6-24.1 3.2-26.4-1.3-2.5-5-3.9-10.5-6.6z" class=""></path>
+                  </svg>
+                </i>
+              </div>
+            </a>
+            <!-- <a class="display_pc" target="_blank" href="https://web.whatsapp.com/send?text=<?php echo base_url(); ?>logo-design-kolhapur">
+              <div class="media-round wa-color-bg ">
+                <i class="">
+                  <svg style="width:18px; padding-bottom:6px;" viewBox="0 0 448 512" ><path fill="currentColor" d="M380.9 97.1C339 55.1 283.2 32 223.9 32c-122.4 0-222 99.6-222 222 0 39.1 10.2 77.3 29.6 111L0 480l117.7-30.9c32.4 17.7 68.9 27 106.1 27h.1c122.3 0 224.1-99.6 224.1-222 0-59.3-25.2-115-67.1-157zm-157 341.6c-33.2 0-65.7-8.9-94-25.7l-6.7-4-69.8 18.3L72 359.2l-4.4-7c-18.5-29.4-28.2-63.3-28.2-98.2 0-101.7 82.8-184.5 184.6-184.5 49.3 0 95.6 19.2 130.4 54.1 34.8 34.9 56.2 81.2 56.1 130.5 0 101.8-84.9 184.6-186.6 184.6zm101.2-138.2c-5.5-2.8-32.8-16.2-37.9-18-5.1-1.9-8.8-2.8-12.5 2.8-3.7 5.6-14.3 18-17.6 21.8-3.2 3.7-6.5 4.2-12 1.4-32.6-16.3-54-29.1-75.5-66-5.7-9.8 5.7-9.1 16.3-30.3 1.8-3.7.9-6.9-.5-9.7-1.4-2.8-12.5-30.1-17.1-41.2-4.5-10.8-9.1-9.3-12.5-9.5-3.2-.2-6.9-.2-10.6-.2-3.7 0-9.7 1.4-14.8 6.9-5.1 5.6-19.4 19-19.4 46.3 0 27.3 19.9 53.7 22.6 57.4 2.8 3.7 39.1 59.7 94.8 83.8 35.2 15.2 49 16.5 66.6 13.9 10.7-1.6 32.8-13.4 37.4-26.4 4.6-13 4.6-24.1 3.2-26.4-1.3-2.5-5-3.9-10.5-6.6z" class=""></path>
+                  </svg>
+                </i>
+              </div>
+            </a> -->
+            <a data-toggle="modal" data-target="#exampleModal">
+              <div class="media-round mail-color-bg" style="">
+                <i class="">
+                  <svg style="width:18px; padding-bottom:6px;" viewBox="0 0 512 512">
+                    <path fill="currentColor" d="M464 64H48C21.49 64 0 85.49 0 112v288c0 26.51 21.49 48 48 48h416c26.51 0 48-21.49 48-48V112c0-26.51-21.49-48-48-48zm0 48v40.805c-22.422 18.259-58.168 46.651-134.587 106.49-16.841 13.247-50.201 45.072-73.413 44.701-23.208.375-56.579-31.459-73.413-44.701C106.18 199.465 70.425 171.067 48 152.805V112h416zM48 400V214.398c22.914 18.251 55.409 43.862 104.938 82.646 21.857 17.205 60.134 55.186 103.062 54.955 42.717.231 80.509-37.199 103.053-54.947 49.528-38.783 82.032-64.401 104.947-82.653V400H48z" class=""></path>
+                  </svg>
+                </i>
+              </div>
+            </a>
+          </div>
+
+          <!-- <?php
+            if($email_msg == 'email_error'){
+          ?>
+          <div class="alert alert-danger" role="alert">
+            Email Not Sent
+          </div>
+          <?php
+            } else if($email_msg == 'email_success'){
+          ?>
+          <div class="alert alert-success" role="alert">
+            <strong>Email Sent Successfully</strong><br />Thank you for submitting your request we will get in touch with you shortly.
+          </div>
+          <?php
+            }
+           ?> -->
+
+
+          <p>If customers only spend 15 seconds on any website, then most of them will love to see something very elegantly designed vs something simple. A website represents your brand and it has the power to make or break your reputation. Basically your efforts of making a strong online presence can’t end by making a basic website.</p>
+          <p>You can call a well-designed website, backbone of your company as it has the capability of keeping customers glued up with your brand. And easy to navigate website, enhances user experience and creates a positive impact about your business.</p>
+          <img class="service-top-image" src="<?php echo base_url(); ?>assets/images/site/website-design-development-kolhapur.jpg" alt="website design, web design, web page design, website development, web design company, web development company, ecommerce website, static website, dynamic website, web design company in kolhapur, website design company in kolhapur, web development company in kolhapur, website development company in kolhapur, web design services" width="100%" >
+          <h3 class="service-sub-title">Why Pixel Bazar</h3>
+
+          <p>We come into picture when you want to give visual presence to your designing ideas. We create websites, which compel customers to take action. Gone are the days when websites were just a URL, as today customers are smart enough to check your website, before actually contacting your brand. We understand the same and create websites which can talk about your business with your target audience. Our team goes that extra mile to make your website work on all platforms like desktop, smartphone, tablet etc. So, make your website advertise 24*7 on behalf of you for adding more and more leads in your pocket.</p>
+          <p>We have a skilled and well-trained web designing team with knowledge of Advance programming languages such as PHP, HTML, CSS, JAVASCRIPT, JQUERY, BOOTSTRAP, WORDPRESS, MYSQL. As a professional web designing  company, we have intense capability in database management and project management. We have a separate team of for website designing and website development.</p>
+          <h3 class="service-sub-title">Website designing with perfection</h3>
+          <!-- <ul class="service-ul">
+            <li>Unique images, delivered on time without any trial error.</li>
+            <li>Designs are created by keeping target audience in mind.</li>
+            <li>You will never face copyright issue.</li>
+          </ul> -->
+          <p>Well! We have earned name and fame in the market because of our perfection. Pixel Bazar strives to meet client's expectations of website design right in the first attempt, hence this reduces trial error and saves customers valuable time. We have a team of experienced designers who have worked with international clients and hence can provide you world-class designing services. Designing needs creativity, and innovation for making it wow for customers. And our designers firstly take a deep dive in customers business for understanding it in a better way. Secondly, they create designs as per the interest of target audience. So, let your customers take interest on your website and allow them to spend more time on it for increasing conversion rate. </p>
+
+            <h3 class="service-sub-title">Web design company in Kolhapur</h3>
+            <p>We have been called as the best web design company in Kolhapur and this title has given to us by our valuable customers. This makes us happy and motivates our team to work flawlessly. We have an experience of serving clients from different domains like education, IT, manufacturing, food industry etc and this has made our team more versatile. </p>
+            <p>So, contact us if you want to add a catchy factor on your website, as our designers are ready to talk with you, regarding your business. Give us a chance and never look back!</p>
+            <a class="btn btn-service btn-outline-warning2  d-none d-sm-block" href="<?php echo base_url(); ?>Contact">Get Started</a>
+            <div class="d-flex justify-content-center d-block d-sm-none">
+              <a class="btn btn-lg btn-outline-warning " href="<?php echo base_url(); ?>Contact">Get Started</a>
+            </div>
+      </div>
+      <div class="col-md-4" id="right-col">
+
+          <h3 class="service-recommended">Recommended for you</h3>
+          <a href="<?php echo base_url(); ?>strategic-branding-kolhapur">
+          <img class="service-pg-img" src="<?php echo base_url(); ?>assets/images/site/branding_in_kolhapur.jpeg" alt="logo design, logo, company logo, business logo, logo creator, company logo design, custom logo design, business logo design, custom logo, best logo design, logo design services " style="width:100%; height:auto;">
+          <p class="red-color service-recommended-head-text" style="font-size:20px;">Transform your brand</p>
+          </a>
+        <div class="service-right-menu">
+          <!-- logo design -->
+        	       <a href="<?php echo base_url(); ?>logo-design-kolhapur">
+                    <div class="row">
+                      <div class="col-6">
+                        <img class="service-img" src="<?php echo base_url(); ?>assets/images/site/logo_design_kolhapur.jpg" alt="Logo design in Kolhapur, Logo Design Company in Kolhapur, Logo Design services in kolhapur, logo, company logo, business logo, logo creator, company logo design, custom logo design, business logo design, custom logo, logo design company in kolhapur, logo printing." >
+                      </div>
+                      <div class="col-6 mt-2">
+                        <p class="service-left-title">Meaningful logos which can stand out! </p>
+                      </div>
+                    </div>
+                  </a>
+
+                  <!-- digital marketing -->
+                          <a href="<?php echo base_url(); ?>digital-marketing-services-kolhapur">
+                            <div class="row">
+                              <div class="col-6">
+                                <img class="service-img" src="<?php echo base_url(); ?>assets/images/site/digital_marketing_company_kolhapur.jpeg" alt="Digital Marketing company in kolhapur, Digital Marketing services in kolhapur, Digital Marketing, seo, search engine optimization, seo services in kolhapur, branding services in kolhapur, facebook marketing, google adwords, ppc, pay per click, twitter marketing, instagram marketing">
+                              </div>
+                              <div class="col-6 mt-2">
+                                <p class="service-left-title">Make your  pocket full of quality leads!</p>
+                              </div>
+                            </div>
+                          </a>
+
+                          <!-- printing services -->
+                                  <a href="<?php echo base_url(); ?>printing-services-kolhapur">
+                                    <div class="row">
+                                      <div class="col-6 ">
+                                        <img class="service-img" src="<?php echo base_url(); ?>assets/images/site/printing_services_kolhapur.jpeg" alt="Printing, Printing Services in Kolhapur, Brochure Printing, Business Card Printing, Flyer Printing, Envelop Printing, books Printing, printing services in kolhapur, printing company">
+                                        </div>
+                                      <div class="col-6 mt-2">
+                                        <p class="service-left-title">High quality prints for highly valuable customers</p>
+                                      </div>
+                                    </div>
+                                  </a>
+
+                                  <!-- corporate events -->
+                                          <a href="<?php echo base_url(); ?>event-management-services-kolhapur">
+                                            <div class="row">
+                                              <div class="col-6">
+                                                <img class="service-img table-cell" src="<?php echo base_url(); ?>assets/images/site/Corporate_eventsS.jpg" alt="Corporate Events, industrial event, events">
+                                                </div>
+                                              <div class="col-6 mt-2">
+                                                <p class="service-left-title">Events which will remain memorable!</p>
+                                              </div>
+                                            </div>
+                                          </a>
+                                          <div class="row">
+
+                                          </div>
+
+          </div>
+          <div class="service-right-menu-bottom">
+            <div class="row" >
+                <img class="service-img" src="<?php echo base_url(); ?>assets/images/portfolio/869_logo.jpg" alt="cool logo designs, graphic design custom logo, company logo, business logo, logo creator">
+                <img class="service-img" src="<?php echo base_url(); ?>assets/images/portfolio/logo_reshimbandhan.jpg" alt="design my logo, design your logo, logo service, brand logo design, new logo design">
+            </div>
+            <div class="row" >
+                <img class="service-img" src="<?php echo base_url(); ?>assets/images/portfolio/734_logo.jpg" alt="business logo design, custom logo, best logo design, logo design services">
+                <img class="service-img" src="<?php echo base_url(); ?>assets/images/portfolio/516_logo.jpg" alt="design my logo, design your logo, logo service, brand logo design, new logo design, corporate logo design">
+            </div>
+          </div>
+      </div>
+    </div>
+  </div>
+  <br><br>
+  <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-lg" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="exampleModalLabel">Share On Email</h5>
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+        <div class="modal-body">
+          <form id="portfolio_form" method="post" class="message-form clear" action="<?php echo base_url(); ?>Welcome/send_service_email">
+            <input type="hidden" name="page_url" value="<?php echo base_url(); ?>website-design-development-kolhapur">
+            <input type="hidden" name="title" value="Beautify you website with Web design and development services">
+            <div class="form-row">
+              <div class="form-group row w-100 ">
+                <label for="inputPassword" class="col-sm-3 col-form-label text-right pb-2">Your Name: </label>
+                <div class="col-sm-8 pb-2">
+                  <input type="text" id="name" name="name" class="valid form-control" placeholder="">
+                </div>
+              </div>
+              <div class="form-group row w-100 pb-1">
+                <label for="inputPassword" class="col-sm-3 col-form-label text-right">Receiver E-mail: </label>
+                <div class="col-sm-8">
+                  <input type="email" id="rec_email" name="rec_email" class="valid form-control" placeholder="">
+                </div>
+              </div>
+            </div>
+          </form>
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-md btn-outline-warning w-25" data-dismiss="modal">Close</button>
+          <button id="send_portfolio_email" type="button" class="btn btn-md btn-outline-warning2 w-25" data-dismiss="modal">Send</button>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div id="snackbar"></div>
+<?php include('footer.php'); ?>
+<script type="text/javascript">
+  $('#send_portfolio_email').click(function(){
+    $('#portfolio_form').submit();
+  });
+  $('#send_portfolio_email').click(function() {
+    $('#portfolio_form').submit();
+  });
+</script>
+<?php if($this->session->flashdata("email_msg")){
+  $email_msg = $this->session->flashdata("email_msg");
+ ?>
+<input type="hidden" id="email_msg" name="email_msg" value="<?php echo $email_msg; ?>">
+<script>
+  $('document').ready(function(){
+    function myFunction() {
+      var x = document.getElementById("snackbar");
+      var email_msg = $('#email_msg').val();
+      if(email_msg == 'email_success'){
+        x.className = "show_success";
+        $('#snackbar').html('Email sent successfully.');
+        setTimeout(function(){ x.className = x.className.replace("show_success", ""); }, 3000);
+        gtag_report_conversion('https://www.pixelbazar.com/');
+      }
+      else{
+        x.className = "show_error";
+        $('#snackbar').html('Email not sent.');
+        setTimeout(function(){ x.className = x.className.replace("show_error", ""); }, 3000);
+      }
+    }
+    myFunction();
+  });
+</script>
+<?php } ?>
+    </body>
+  </html>
